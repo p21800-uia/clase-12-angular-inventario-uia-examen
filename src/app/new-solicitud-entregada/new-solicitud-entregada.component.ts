5ea07d5e5171c2eec9502ad675f3fc93a5aa0dfc
p21800-uia <p21800@correo.uia.mx>
2023-04-02 11:31:24 -0600

    first commit

---
+++ src/app/new-solicitud-entregada/new-solicitud-entregada.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-solicitud-entregada',
+  templateUrl: './new-solicitud-entregada.component.html',
+  styleUrls: ['./new-solicitud-entregada.component.css']
+})
+export class NewSolicitudEntregadaComponent {
+
+}
